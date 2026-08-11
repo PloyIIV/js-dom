8128f73e2168d8980386d807abfa21e3fb8c1263
PloyIIV <132745974+PloyIIV@users.noreply.github.com>
2026-08-12 01:01:48 +0700

    feat: add click for settimeout

--- cookie-clicker/script.js
+++ cookie-clicker/script.js
@@ -1,9 +1,15 @@
+document.querySelector('.click-count').textContent = localStorage.getItem('count')
+let countClick = 1;
+let expCount = 0;
+
+const powerClick = document.querySelector('.power-count')
+powerClick.textContent = countClick
 const character = document.querySelector('.character')
 character.addEventListener('click', (e) => {
     const countLogo = document.createElement('div')
     countLogo.className = 'count-logo'
     let localCount = Number(localStorage.getItem("count"))
-    localCount = localCount + 1
+    localCount = localCount + countClick
     if (localStorage.getItem("count")) {
         localStorage.setItem('count', localCount)
     } else {
@@ -17,9 +23,57 @@ character.addEventListener('mousedown', () => {
 character.addEventListener('mouseup', () => {
     document.querySelector('.character').src = './assets/char1.gif'
 })
-const skillDiv = document.querySelector('.skill-div')
-skillDiv.addEventListener('click', () => {
 
+// RESET ALL
+const resetCount = document.querySelector('.status-info button')
+resetCount.addEventListener('click', () => {
+    localStorage.removeItem('count')
+    document.querySelector('.click-count').textContent = 0
+    document.querySelector('.exp').textContent = 0
+    countClick = 1
+    powerClick.textContent = countClick
 })
 
-// const fireflyImg = 'https://static.vecteezy.com/system/resources/thumbnails/070/959/373/small/pixel-art-firefly-with-glowing-abdomen-on-transparent-background-illustration-png.png'
+
+// SKILLS
+const firefly = document.querySelector('.firefly')
+const slime = document.querySelector('.slime')
+const snail = document.querySelector('.snail')
+const mushroom = document.querySelector('.mushroom')
+const trunk = document.querySelector('.trunk')
+
+snail.addEventListener('click', () => {
+    if(localStorage.getItem('count') >= 10) {
+        localStorage.setItem('count', localStorage.getItem('count') - 10)
+        countClick = countClick + 1
+    }
+    document.querySelector('.click-count').textContent = localStorage.getItem('count')
+    powerClick.textContent = countClick
+})
+
+slime.addEventListener('click', () => {
+    if(localStorage.getItem('count') >= 50) {
+        localStorage.setItem('count', localStorage.getItem('count')-50)
+        countSlime();
+        document.querySelector('.click-count').textContent = localStorage.getItem('count')
+    }
+})
+
+mushroom.addEventListener('click', () => {
+    if(localStorage.getItem('count') >= 500) {
+        localStorage.setItem('count', localStorage.getItem('count')-500)
+        countMushroom();
+        document.querySelector('.click-count').textContent = localStorage.getItem('count')
+    }
+})
+
+function countSlime() {
+    expCount = expCount + 1
+    document.querySelector('.exp-count').textContent = expCount
+    setTimeout(countSlime, 1000);
+}
+function countMushroom() {
+    expCount = expCount + 50
+    document.querySelector('.exp-count').textContent = expCount
+    setTimeout(countMushroom, 1000);
+}
